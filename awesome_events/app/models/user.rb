878e0c983f9ceab4016b34b07813c44f63bf24f4
yuta.sano <snyt45@gmail.com>
2021-10-29 09:19:52 +0900

    退会処理を追加

--- awesome_events/app/models/user.rb
+++ awesome_events/app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ApplicationRecord
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
+  before_destroy :check_all_events_finished
+
+  # nullify: User削除時に関連レコードの外部キーをnullにする
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participating_events, through: :tickets, source: :event
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "公開中の未終了イベントが存在します。"
+    end
+
+    if participating_events.where(":now < end_at", now: now).exists?
+      errors[:base] << "未終了の参加イベントが存在します。"
+    end
+
+    # エラーがある場合は削除処理を中断する
+    throw(:abort) unless errors.empty?
+  end
 
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
