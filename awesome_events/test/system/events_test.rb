9567516749dd8453cc782c39f35789aed78cf34f
yuta.sano <snyt45@gmail.com>
2021-11-06 09:20:30 +0900

    イベントページを表示するテストを追加

--- awesome_events/test/system/events_test.rb
+++ awesome_events/test/system/events_test.rb
@@ -1,6 +1,13 @@
 require "application_system_test_case"
 
 class EventsTest < ApplicationSystemTestCase
+  test "/events/new ページを表示" do
+    sign_in_as(FactoryBot.create(:user))
+
+    visit new_event_url
+    assert_selector "h1", text: "イベント作成"
+  end
+
   test "/events/:id ページを表示" do
     event = FactoryBot.create(:event)
     visit event_url(event)
