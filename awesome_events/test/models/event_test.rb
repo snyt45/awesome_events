2a6469633b80a4a7a2690b6ba0aedad9eff85c8d
yuta.sano <snyt45@gmail.com>
2021-11-12 09:25:48 +0900

    event.created_by?のテストを追加

--- awesome_events/test/models/event_test.rb
+++ awesome_events/test/models/event_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#created_by? owner_id と 引数の#idが同じとき' do
+    event = FactoryBot.create(:event)
+    assert_equal(true, event.creaeted_by?(event.owner))
+  end
+  test '#created_by? owner_id と 引数の#idが異なるとき' do
+    event = FactoryBot.create(:event)
+    another_user = FactoryBot.create(:user)
+    assert_equal(false, event.creaeted_by?(another_user))
+  end
+  test '#created_by? nil なとき' do
+    event = FactoryBot.create(:event)
+    assert_equal(false, event.creaeted_by?(nil))
+  end
 end
