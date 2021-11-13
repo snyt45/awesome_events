9ec1d12095f8699f0e8380e00cc5cadb72705de3
yuta.sano <snyt45@gmail.com>
2021-11-13 13:59:46 +0900

    スタブを使ってテスト

--- awesome_events/test/models/event_test.rb
+++ awesome_events/test/models/event_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   test '#created_by? owner_id と 引数の#idが同じとき' do
     event = FactoryBot.create(:event)
-    assert_equal(true, event.creaeted_by?(event.owner))
+    user = User.new
+    user.stub(:id, event.owner_id) do
+      assert_equal(true, event.creaeted_by?(user))
+    end
   end
   test '#created_by? owner_id と 引数の#idが異なるとき' do
     event = FactoryBot.create(:event)
