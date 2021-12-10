1ea50496b0e4a56b4ffbd5bd7f062f18acfb35c1
yuta.sano <snyt45@gmail.com>
2021-12-10 09:40:25 +0900

    elasticsearch導入
    sudo sysctl --w vm.max_map_count=262144
    elacticsearchが起動している必要がある。

--- awesome_events/app/javascript/packs/application.js
+++ awesome_events/app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 import "bootstrap"
 import "bootstrap/scss/bootstrap.scss"
+require("get_form_turbolinks")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
